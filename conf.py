d30d9cdf170b97c173dca542d39a24a810206991
Vladimir Rutsky <altsysrq@gmail.com>
2014-02-26 01:02:25 +0400

    Add env/ dir to Sphinx ignore list

--- conf.py
+++ conf.py
@@ -75,7 +75,7 @@ release = '2014.02.18'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'handout', 'common']
+exclude_patterns = ['env', '_build', 'handout', 'common']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
