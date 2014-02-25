7a2fa12b0d5a132a27819dc24c4859c3cfa4fb8b
Vladimir Rutsky <altsysrq@gmail.com>
2014-02-26 00:34:10 +0400

    Use Python 3 doc when refering to Python documentation

--- conf.py
+++ conf.py
@@ -292,4 +292,4 @@ slide_theme_options = {
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {'python': ('http://docs.python.org/3/', None)}
