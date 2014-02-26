8c003d83d6593573a0dca9f4c4f0969ca56d9758
Vladimir Rutsky <altsysrq@gmail.com>
2014-02-26 04:21:05 +0400

    Use custom development version of Hieroglyph

--- bootstrap-env.sh
+++ bootstrap-env.sh
@@ -5,4 +5,4 @@ python3 -m venv env
 source env/bin/activate
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python
 easy_install pip
-pip install hieroglyph==0.6.5.dev
+pip install git+git://github.com/rutsky/hieroglyph.git@py2014
