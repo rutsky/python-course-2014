ec07dbca44bb6fd1a203c441eb93a4961dee3a19
Vladimir Rutsky <altsysrq@gmail.com>
2014-03-22 15:19:36 +0400

    lecture 5

---
+++ 05_oop_classes/conf.py
@@ -0,0 +1,32 @@
+import sys
+import os
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath('../common/conf/'))
+
+from presentation_common import *
+
+# -- General configuration ------------------------------------------------
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = '22.03.2014'
+# The full version, including alpha/beta/rc tags.
+release = '22.03.2014'
+
+# There are two options for replacing |today|: either, you set today to some
+# non-false value, then it is used:
+today = '22.03.2014'
+# Else, today_fmt is used as the format for a strftime call.
+#today_fmt = '%B %d, %Y'
+
+
+# Place custom static assets in the _static directory and uncomment
+# the following lines to include them
+
+slide_footer = slide_footer_template.format(today)
